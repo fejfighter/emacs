e53ab2e05e37461d1523fc8a2004ea8bcce63fe3
Jeff Walsh <fejfighter@gmail.com>
2021-08-14 22:35:31 +1000

    * src/alloc.c (sweep_obarray): Scan global reference

--- src/alloc.c
+++ src/alloc.c
@@ -8184,6 +8184,8 @@ gc_phase_plan_sweep (void)
 void
 sweep_obarray (void)
 {
+  scan_reference (&(Vobarray), GC_PHASE_SWEEP);
+
   const Lisp_Object obarray = Vobarray;
   const size_t obsize = ASIZE (obarray);
 
